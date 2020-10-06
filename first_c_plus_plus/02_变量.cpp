5ac376b6477ec99e9e0ce9920ce036c8ebacb661
Sen <12345678>
2020-10-06 22:42:46 +0800

    初始提交

---
+++ first_c_plus_plus/02_变量.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main2()
+{
+	int a = 10;
+	cout << "±äÁ¿a = " << a << endl;
+
+	system("pause");
+
+	return 0;
+}
---
+++ first_c_plus_plus/03_常量.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// 定义宏常量
+#define Day 7
+
+int main() 
+{
+	cout << "一周有多少天：" << Day << endl;
+	system("pause");
+
+	return 0;
+}
